0100e63e4aa24e0d053053762098d6fcad631426
lourw <56288712+lourw@users.noreply.github.com>
2024-10-26 00:46:52 -0700

    chore(repo): add nx

---
+++ eslint.config.js
@@ -0,0 +1,34 @@
+import nx from '@nx/eslint-plugin';
+const { configs } = nx
+
+export default [
+  ...configs['flat/base'],
+  ...configs['flat/typescript'],
+  ...configs['flat/javascript'],
+  {
+    ignores: ['**/dist'],
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    rules: {
+      '@nx/enforce-module-boundaries': [
+        'error',
+        {
+          enforceBuildableLibDependency: true,
+          allow: ['^.*/eslint(\\.base)?\\.config\\.[cm]?js$'],
+          depConstraints: [
+            {
+              sourceTag: '*',
+              onlyDependOnLibsWithTags: ['*'],
+            },
+          ],
+        },
+      ],
+    },
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    // Override or add rules here
+    rules: {},
+  },
+];
